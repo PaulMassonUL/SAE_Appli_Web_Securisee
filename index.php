782c5470648dabcf94294e61d8c13ae78d3b870e
unknown <Cedran-Bastien>
2022-11-07 16:39:03 +0100

    lancement application

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once 'vendor/autoload.php';
+use netvod\dispatch\Dispatcher_Auth as Dispatcher_Auth;
+
+$dispatch = new Dispatcher_Auth();
+$dispatch->run();
 
 $episode = new \netvod\video\Episode("ressources/beach.mp4","titre","resume",10.00,false);
 $episodeRender = new \netvod\render\EpisodeRenderer($episode);
