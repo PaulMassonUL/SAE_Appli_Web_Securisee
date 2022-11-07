448c2dc576bc0db774804e2725be21324ae09e15
toinou887 <rionde8u@etu.univ-lorraine.fr>
2022-11-07 15:22:12 +0100

    ajout de la classe User et modif de la classe Serie

---
+++ src/user/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace netvod\user;
+
+use netvod\video\Catalogue;
+
+class User
+{
+    /**
+     * email de l'utilisateur
+     */
+    protected string $email;
+
+    /**
+     * mot de passe de l'utilisateur
+     */
+    private string $passwd;
+
+    /**
+     * catalogue de l'utilisateur
+     */
+    private Catalogue $catalogue;
+
+    /**
+     * liste de series preferées de l'utilisateur
+     */
+    private Catalogue $seriesPref;
+
+    /**
+     * @param $eml email
+     * @param $pwd mot de passe
+     */
+    public function __construct($eml, $pwd)
+    {
+        $this->email = $eml;
+        $this->passwd = $pwd;
+    }
+
+    /**
+     * @return Catalogue
+     */
+    public function getCatalogue(): Catalogue
+    {
+        return $this->catalogue;
+    }
+
+    /**
+     * @return Catalogue
+     */
+    public function getSeriesPref(): Catalogue
+    {
+        return $this->seriesPref;
+    }
+
+}
