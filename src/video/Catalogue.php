daec377f6d9e58ebfb824e1475ccbc367cb73a7a
PaulMassonUL <paul.masson1@etu.univ-lorraine.fr>
2022-11-07 16:31:08 +0100

    PHPDoc Catalogue

--- src/video/Catalogue.php
+++ src/video/Catalogue.php
@@ -6,18 +6,34 @@ use iutnc\deefy\exception\InvalidPropertyNameException;
 
 class Catalogue {
 
+    /**
+     * Series of the catalog
+     */
     private array $series;
 
+    /**
+     * constructor initializes the series array
+     */
     public function __construct()
     {
         $this->series = [];
     }
 
+    /**
+     * Add a serie to the catalog
+     * @param $s the serie to add
+     */
     public function ajouterSerie(Serie $s) : void
     {
         $this->series[] = $s;
     }
 
+    /**
+     * getter
+     * @param $attrname attribute name
+     * @return mixed attribute value
+     * @throws InvalidPropertyNameException if the attribute name is invalid
+     */
     public function __get($attname) {
         if (property_exists($this, $attname)) return $this->$attname;
         throw new InvalidPropertyNameException("Nom d'attribut invalide : $attname");
