f1610abbbcaa9c0bd9c7cc42fd65958cd5032738
toinou887 <rionde8u@etu.univ-lorraine.fr>
2022-11-07 17:13:21 +0100

    (modifie par paul) modif de la methode renderCompact de la classe SerieRenderer

--- src/render/SerieRenderer.php
+++ src/render/SerieRenderer.php
@@ -46,16 +46,17 @@ class SerieRenderer implements Renderer
 
     public function renderDetail() : string
     {
-        $html = "<h1>{$this->serie->titre}</h1>";
-        $html .= "<h2>{$this->serie->genre} - {$this->serie->public}</h2>";
-        $html .= "<p>$this->serie->descriptif</p>";
-        $html .= "<p><<strong>Annee de sortie : </strong>$this->serie->anneeSortie<strong> - Date d ajout : $this->serie->dateAjout</strong>";
+        $html = "<h1>{$this->serie->getTitre()}</h1>";
+        $html .= "<h2>{$this->serie->getGenre()} - {$this->serie->getPublic()}</h2>";
+        $html .= "<p>$this->serie->getDescriptif()</p>";
+        $html .= "<p><strong>Annee de sortie : </strong>$this->serie->getAnneeSortie()<strong> - Date d ajout : $this->serie->getDateAjout()</strong>";
         $html .= "<p>Nb episodes :". count($this->serie->getEpisodes())."</p>";
 
+       $html .= "<ol>";
         foreach ($this->serie as $key=>$ep) {
-            
-
-
+            $html .= "<li><strong>Episode $key</strong>";
+            $epRend = new EpisodeRenderer($ep);
+            $html .= "{$epRend->render(Renderer::DETAIL)}</li>";
         }
         $html .= "</ol>";
         return $html;
