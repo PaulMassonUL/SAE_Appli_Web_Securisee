52643af708f19aa55e1dbe34f995ed2704696025
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2022-11-08 11:31:19 +0100

    ajoute commentaire Catalogue.php

--- src/video/Catalogue.php
+++ src/video/Catalogue.php
@@ -6,19 +6,37 @@ use iutnc\deefy\exception\InvalidPropertyNameException;
 
 class Catalogue
 {
-
+    /**
+     * @var array
+     * correspond à la liste des séries
+     */
     private array $series;
 
+    /**
+     * @param array $series
+     * Constructeur paramétré
+     */
     public function __construct(array $series)
     {
         $this->series = $series;
     }
 
+    /**
+     * @param Serie $s
+     * @return void
+     * Ajoute une série à la liste
+     */
     public function ajouterSerie(Serie $s): void
     {
         $this->series[] = $s;
     }
 
+    /**
+     * @param $attname
+     * @return mixed
+     * @throws InvalidPropertyNameException
+     * Getter
+     */
     public function __get($attname)
     {
         if (property_exists($this, $attname)) return $this->$attname;
