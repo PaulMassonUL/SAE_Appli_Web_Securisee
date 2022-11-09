8493f5fa835cf433e5568dac601ee1b6f7208b02
toinou887 <rionde8u@etu.univ-lorraine.fr>
2022-11-09 11:45:22 +0100

    modification de la div notation

--- src/render/SerieRenderer.php
+++ src/render/SerieRenderer.php
@@ -69,6 +69,7 @@ class SerieRenderer implements Renderer
             </div>
             
             <div id="notation">
+                <br>
                 <label> Commentaire : <input type="text" name="commentaire" placeholder="Entrer un commentaire" > </label>
                 <label> Note : <input type="number" name="note" placeholder="Entrer une note de 1 a 5" > </label>
             </div>
