df3662ea4420dfca32c813414f1fc4ee7f30e123
toinou887 <rionde8u@etu.univ-lorraine.fr>
2022-11-10 10:28:43 +0100

    modif du tri du catalogue v1

--- src/action/ShowCatalogAction.php
+++ src/action/ShowCatalogAction.php
@@ -31,8 +31,13 @@ class ShowCatalogAction extends Action
     public function execute(): string
     {
         $renderer = new CatalogueRenderer($this->catalogue);
-
-        $html = '<form method="post" action="?action=show-serie-details">';
+        $html = '<div id="triChoix"> <p> Quel tri voulez vous prendre ? : </p> ';
+        $html .= '<ol> <ul> par defaut : 0 </ul> <ul> par titre : 1 </ul> <ul> par date : 2 </ul> <ul> par Nb d\'episodes : 3 </ul> <ul> par noteMoyenne : 4 </ul> </ol>';
+        $html .= '<form id="choix" action = "?action=addChoiceTriCatalogue" method="post">';
+        $html .= '<input type="number" name="choixTri" placeholder="0-4" min="0" max="4" > <button type="submit"> Valider </button>';
+        $html .= '</form>';
+        $html .= '</div>';
+        $html .= '<form method="post" action="?action=show-serie-details">';
         $html .= $renderer->render(Renderer::COMPACT);
         $html .= '</form>';
         return $html;
--- src/render/CatalogueRenderer.php
+++ src/render/CatalogueRenderer.php
@@ -26,13 +26,7 @@ class CatalogueRenderer implements Renderer
         $html .= '<div><label id="title">' . $nom . '</label></div>';
         $html .= '<div id="catalog">';
 //        $this->catalogue->definirTri(Catalogue::TRI_NORMAL);
-        $tri = "";
-        $html .= '<div id="triChoix"> <p> Quel tri voulez vous prendre ? : </p> ';
-        $html .= '<ol> <ul> par defaut : 0 </ul> <ul> par titre : 1 </ul> <ul> par date : 2 </ul> <ul> par Nb d\'episodes : 3 </ul> <ul> par noteMoyenne : 4 </ul> </ol>';
-        $html .= '<form id="choix" action = "?action=addChoiceTriCatalogue" method="post">';
-        $html .= '<input type="number" name="choixTri" placeholder="0-4" min="0" max="4" > <button type="submit"> Valider </button>';
-        $html .= '</form>';
-        $html .= '</div>';
+
         foreach ($this->catalogue->__get("series") as $serie) {
             $renderer = new SerieRenderer($serie);
             $html .= $renderer->render(Renderer::COMPACT);
